141e1bd9cc22e787b6ae41476a7ee7324dd38fd2
Regis <boudinot.regis@yahoo.com>
2015-07-20 10:34:23 -0600

    Better test

--- test/output_test.rb
+++ test/output_test.rb
@@ -13,7 +13,10 @@ class OutputTest < Minitest::Test
     ARGV[0] = './my_input.md'
     ARGV[1] = './my_output.html'
     input_for_test = Output.new
-    expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+    expected = input_line_array = []
+    File.foreach(ARGV[0]).map.with_index do |line, line_num|
+       input_line_array << line_num
+    end
     assert_equal expected, input_for_test.input_line_read
   end
 
@@ -21,7 +24,10 @@ class OutputTest < Minitest::Test
     ARGV[0] = './my_input.md'
     ARGV[1] = './my_output.html'
     input_for_test = Output.new
-    expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39]
+    expected = output_line_array = []
+      File.foreach(ARGV[1]).map.with_index do |line, line_num|
+         output_line_array << line_num
+      end
     assert_equal expected, input_for_test.output_line_read
   end
 
@@ -29,7 +35,7 @@ class OutputTest < Minitest::Test
     ARGV[0] = 'my_input.md'
     ARGV[1] = 'my_output.html'
     input_for_test = Output.new
-    expected = "Converted my_input.md (25 lines) to my_output.html (39 lines)"
+    expected = "Converted #{ARGV[0]} (#{input_for_test.input_line_read.last} lines) to #{ARGV[1]} (#{input_for_test.output_line_read.last} lines)"
     assert_equal expected, input_for_test.terminal_output
   end
 
