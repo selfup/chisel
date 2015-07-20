9167d9fbccd8f0b622ea8ad80183fc7f0716d985
Regis <boudinot.regis@yahoo.com>
2015-07-19 19:49:01 -0600

    Made more logical test to chisel_test.rb

--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -8,18 +8,25 @@ class ChiselTest < Minitest::Test
     assert Chisel
   end
 
+  def test_it_can_read_the_input
+    input_1 = ARGV[0]
+    input_for_test = Chisel.new(input_1)
+    expected = File.read(input_1)
+    assert_equal expected, input_for_test.input
+  end
+
   def test_it_can_split_the_input
     input_1 = ARGV[0]
     input_for_test = Chisel.new(input_1)
-    expected =
-["# My Life in Desserts", "## Chapter 1: The Beginning", "\"You just *have* to try the cheesecake,\" he said. \"Ever since it appeared in
-**Food & Wine** this place has been packed every night.\"", "My favorite cuisines are:", "* Sushi
-* Barbecue
-* Mexican", "\"Boo!\"", "My *emphasized and **stronged** text* is awesome.", "# SCARY!", "##### Super spooky!", "1. French
-1. Thai
-1. Japanese", "Yay!
-"]
+    expected = input_for_test.input.split("\n\n")
     assert_equal expected, input_for_test.split_it_up
+  end
+
+  def test_it_can_join_the_logic
+    input_1 = ARGV[0]
+    input_for_test = Chisel.new(input_1)
+    expected = input_for_test.assemble
+    assert_equal expected, input_for_test.logic_the_things.join("\n\n")
   end
 
   def test_it_can_stylize_the_logic
